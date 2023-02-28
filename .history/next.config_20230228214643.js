e25f67a7a007c1df449fd55df1b7e723f7df3362
Wenzhang Ge <2281116071@qq.com>
2023-02-28 21:47:02 +0800

    忽视运行时警告（因为本地正常工作但无法在Vercel上Build😅）

--- .history/next.config_20230228214643.js
+++ .history/next.config_20230228214643.js
@@ -6,7 +6,7 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // 我必须添加这个才能让一个在本地正常工作的项目成功在Vercel上构建 😅    
+    // 我必须添加这个才能让一个在本地正常工作的项目成功在Vercel上构建 😅
     ignoreBuildErrors: true
   }
 }
